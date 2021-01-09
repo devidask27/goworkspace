5b462b01a8c0e2c5d4f3ea1949d36e172f8c9a60
devidas <devidas@platform9.com>
2021-01-09 20:47:12 +0530

    struct example

---
+++ struct.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	type info struct{
+		name string
+		phoneNumber int
+	}
+	var myInfo info
+	myInfo.name = "devi"
+	myInfo.phoneNumber = 123
+	fmt.Println(myInfo)
+}
